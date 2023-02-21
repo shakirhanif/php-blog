1126a3de818d7fdc444b7832415be29ef0f391cc
Shakir Hanif <shakirhanif@hotmail.com>
2023-02-21 19:12:20 +0500

    some minor changes to delete own

--- posts/delete.php
+++ posts/delete.php
@@ -1,10 +1,18 @@
 <?php
     require "../config/config.php";
+    session_start();
     if (isset($_GET['post_id'])) {
         $did=$_GET['post_id'];
-        $post = $conn->prepare("delete from blog.posts where id=:id");
-        $post->execute([':id'=>$did]);
-        header('location: http://localhost/blog');
+        $postOwner = $conn->prepare("select * from blog.posts where id=$did");
+        $postOwner->execute();
+        $postRow = $postOwner->fetch(PDO::FETCH_ASSOC);
+        if ($postRow['user_id']==$_SESSION['user_id']) {
+            $postDelete = $conn->prepare("delete from blog.posts where id=:id");
+            $postDelete->execute([':id'=>$did]);
+            header('location: http://localhost/blog');
+        }else{
+            echo "you can only delete your own post";
+        }
     }
     
 ?>
--- posts/post.php
+++ posts/post.php
@@ -36,8 +36,10 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <p><?php echo $row['body']; ?> </p>
+                        <?php if($_SESSION['user_id']==$row['user_id']): ?>
                         <a href="<?php echo "$URL/posts/delete.php?post_id=$id" ?>" class="btn btn-danger text-center float-end">DELETE</a>
-                        <a href="" class="btn btn-warning text-center">UPDATE</a>
+                        <a href="<?php echo "$URL/posts/update.php?post_id=$id" ?>" class="btn btn-warning text-center">UPDATE</a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
