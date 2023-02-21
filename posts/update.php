1d38c363aeb4b5b5c66425bfe40426c6a2610e68
Shakir Hanif <shakirhanif@hotmail.com>
2023-02-22 00:21:13 +0500

    added prvw scpt very imp updates

--- posts/update.php
+++ posts/update.php
@@ -1,6 +1,35 @@
 <?php require "../partials/navbar.php"; ?>
+<?php require '../config/config.php'; ?>
+<?php
+    $post_id = $_GET['post_id'];
+    $qry = $conn->prepare("select * from blog.posts where id=$post_id "); 
+    $qry->execute();
+    $row = $qry->fetch(PDO::FETCH_ASSOC);
+    $imageName = $row['image'];
+    if (isset($_POST['submit'])) {
+        if (!$_FILES['image']['name']=='') {
+            $img = date("his").$_FILES['image']['name'];
+        }else{
+            $img = $imageName;
+        };
+        $dir = '../images/' . basename($img);
+        $update = $conn->prepare("update blog.posts set title=:title,subtitle=:subtitle,body=:body,image=:image where id=$post_id ");
+        $update->execute([
+            ':title'=>$_POST['title'],
+            ':subtitle'=>$_POST['subtitle'],
+            ':body'=>$_POST['body'],
+            ':image'=>$img,
+        ]);
+        if (move_uploaded_file($_FILES['image']['tmp_name'],$dir)) {
+            unlink("../images/$imageName");
+            header("location: $URL/posts/post.php?post_id=$post_id");
+          }
+        header("location: $URL/posts/post.php?post_id=$post_id");
+    }
+
+ ?>
         <!-- Page Header-->
-        <header class="masthead" style="background-image: url('../assets/img/home-bg.jpg')">
+        <header class="masthead" style="background-image: url(<?php echo !$imageName==''? "http://localhost/blog/images/$imageName" :'../assets/img/home-bg.jpg';?>)">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
@@ -13,27 +42,27 @@
             </div>
         </header>
        
-                <!-- Main Content-->
         <div class="container px-4 px-lg-5">
 
-            <form method="POST" action="">
-              <!-- Email input -->
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']."?post_id=$post_id"; ?>" enctype="multipart/form-data">
               <div class="form-outline mb-4">
-                <input type="text" name="email" id="form2Example1" class="form-control" placeholder="title" />
+                <input value="<?php echo $row['title']; ?>" type="text" name="title" id="form2Example1" class="form-control" placeholder="title" />
                
               </div>
 
               <div class="form-outline mb-4">
-                <input type="text" name="email" id="form2Example1" class="form-control" placeholder="subtitle" />
+                <input value="<?php echo $row['subtitle']; ?>" type="text" name="subtitle" id="form2Example1" class="form-control" placeholder="subtitle" />
             </div>
 
               <div class="form-outline mb-4">
-                <input type="text" name="email" id="form2Example1" class="form-control" placeholder="body" />
+                <textarea name="body" id="form2Example1" cols="30" rows="10" style="width:100%"><?php echo $row['body']; ?></textarea>
             </div>
-
-              
+            
+            <div id="preview" ><img src="" width="500px" id="output" ></div>
+            
+            <img src="" alt="" srcset="">
              <div class="form-outline mb-4">
-                <input type="file" name="email" id="form2Example1" class="form-control" placeholder="image" />
+                <input type="file" name="image"   id="image" onchange="loadFile(event)" class="form-control" placeholder="image" />
             </div>
 
 
@@ -46,45 +75,13 @@
 
            
         </div>
-    <!-- Footer-->
-        <footer class="border-top">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <ul class="list-inline text-center">
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="../js/script.js"></script>
-    </body>
-</html>
+        <script>
+            function loadFile(e) {
+                let output = document.getElementById('output');
+                output.src = URL.createObjectURL(e.target.files[0]);
+                output.onload = function() {
+                    URL.revokeObjectURL(output.src) // free memory
+                }
+            }
+        </script>
+   <?php require "../partials/footer.php";?>
